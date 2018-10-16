40e291c3c6dc5fb8f153d25d17b0c56409421a4d
fenghome <feng_home@126.com>
2018-10-16 17:17:32 +0800

    完善地址选择的算法

--- src/components/Common/AddressSelect/AddressSelect.js
+++ src/components/Common/AddressSelect/AddressSelect.js
@@ -3,62 +3,123 @@ import { Icon } from 'antd';
 import style from './AddressSelect.less';
 
 class AddressSelect extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      isSelect:false,
-      address:['河北省','石家庄市','新华区']
+    this.state = {
+      isSelect: false,
+      address: [],
+      tabs:[],
+      selectTabIndex: -1,
+      selectAddressTreeIndex:[-1,-1,-1,-1],
+      selectItems: props.addressTree.selectItems,
     }
   }
 
-  mouseEnterAddress=()=>{
+  mouseEnterAddress = () => {
     this.setState({
-      isSelect:true
+      isSelect: true
     })
   }
 
-  mouseLeaveAddress=()=>{
+  mouseLeaveAddress = () => {
     this.setState({
-      isSelect:false
+      isSelect: false
     })
   }
+
+  onSelectAddress = (item) => {
+    let { isSelect, address,selectTabIndex , selectItems } = this.state;
+
+    //change selectTabIndex
+    if (item.sub && address[0] != '请选择') {
+      this.setState({
+        selectTabIndex: selectTabIndex + 1
+      })
+    }
+    //change adress
+    if (address[0] == '请选择') {
+      address[0] = item.name;
+    } else{
+      address.push(item.name);
+    }
+
+    console.log(address);
+    //change selectItems,isSelect
+    if (item.sub) {
+      selectItems = item.sub;
+    } else {
+      isSelect = false;
+    }
+
+    this.setState({
+      isSelect,
+      address,
+      selectItems
+    })
+  }
+
+  changeSelectTabIndex = (index) => {
+    // const { addressTree } = this.props;
+    this.setState({
+      selectTabIndex: index
+    })
+    console.log(index);
+  }
+
   render() {
     const { citys } = this.props;
-    
+
     return (
       <div>
-        <div 
+        <div
           onMouseEnter={this.mouseEnterAddress}
           onMouseLeave={this.mouseLeaveAddress}
-          className={this.state.isSelect?style.addressLineSelect:style.addressLine}>
-          <a 
-          >请选择地址<Icon type="down" theme="outlined" style={{ marginLeft: 6 }} /></a>
+          className={this.state.isSelect ? style.addressLineSelect : style.addressLine}>
+          {
+            this.state.address[0] == '请选择' ?
+              <a>请选择地址<Icon type="down" theme="outlined" style={{ marginLeft: 6 }} /></a>
+              :
+              <a>
+                {
+                  this.state.address.map((item, index) => (
+                    <span key={index}>{item}</span>
+                  ))
+                }
+              </a>
+          }
         </div>
         {
           this.state.isSelect && (
-          <div 
-            className={style.selectBody}
-            onMouseEnter={this.mouseEnterAddress}
-            onMouseLeave={this.mouseLeaveAddress}
-          >
-            <div>
-              {
-                this.state.address.map(a=>(
-                  <div className={style.tab}>{a}</div>
-                ))
-              }
-              {/* <div className={style.selectTab}><a>请选择</a></div>
+            <div
+              className={style.selectBody}
+              onMouseEnter={this.mouseEnterAddress}
+              onMouseLeave={this.mouseLeaveAddress}
+            >
+              <div>
+                {
+                  this.state.address.map((a, index) => {
+                    let styleObj = this.state.selectTabIndex == index ? style.selectTab : style.tab;
+                    return (
+                      <div className={styleObj} key={index} onClick={() => this.changeSelectTabIndex(index)}>
+                        <a>{a}</a>
+                      </div>
+                    )
+                  })
+                }
+                {/* <div className={style.selectTab}><a>请选择</a></div>
               <div className={style.tab}>石家庄</div> */}
-              <div className={style.hr} /> 
+                <div className={style.hr} />
+              </div>
+              <div className={style.addressInfoWrap}>
+                {
+                  this.state.selectItems.map((item, index) => (
+                    <div className={style.addressInfoItem} key={item.key}>
+                      <a onClick={() => this.onSelectAddress(item, index)}>{item.name}</a>
+                    </div>
+                  ))
+                }
+              </div>
             </div>
-            <div className={style.addressInfoWrap}>
-              {
-                citys.map(city=>(
-                  <div className={style.addressInfoItem}><a>{city.name}</a></div>
-                ))
-              }
-            </div>
-          </div>
           )
         }
       </div>
--- src/components/ProductInfo/ProductInfo.js
+++ src/components/ProductInfo/ProductInfo.js
@@ -4,39 +4,63 @@ import Tag from '../Common/Tag/Tag';
 import Coupon from '../Common/Coupon/Coupon';
 import AddressSelect from '../Common/AddressSelect/AddressSelect';
 
-const citys = [
-  {
-  key:'hebei',
-  name:'河北省',
-  citys:[
+const addressTree = {
+  tabName: '请选择', selectItems: [
     {
-      key:'shijiazhuang',
-      name:'石家庄',
-      qu:[
-        {
-          key:'xinhua',
-          name:'新华区',
-          jiedao:[
-            {
-              key:'gexin',
-              name:'革新街道',
-            },
-            {
-              key:'beiyuan',
-              name:'北苑街道'
-            },
-            {
-              key:'ningan',
-              name:'宁安街道'
+      key: 'hebei',
+      name: '河北省',
+      sub: {
+        tabName: '城市', selectItems: [
+          {
+            key: 'sjz', name: '石家庄', sub: {
+              tabName: '区县', selectItems: [
+                {
+                  key: 'xh', name: '新华区', sub: {
+                    tabName: '街道', selectItems: [
+                      { key: 'xhl', name: '新华路街道' },
+                      { key: 'gx', name: '革新街道' }
+                    ]
+                  }
+                },
+                {
+                  key: 'qd', name: '桥东区', sub: {
+                    tabName: '街道', selectItems: [
+                      { key: 'wmj', name: '维明街道' },
+                      { key: 'yy', name: '友谊街道' }
+                    ]
+                  }
+                }
+              ]
             }
-          ]
-        },
-      ]
+          },
+          {
+            key: 'bd', name: '保定', sub: {
+              tabName: '区县', selectItems: [
+                {
+                  key: 'ax', name: '安新县', sub: {
+                    tabName: '村庄', selectItems: [
+                      { key: 'wjz', name: '王家寨' },
+                      { key: 'pc', name: '彭村' }
+                    ]
+                  }
+                },
+                {
+                  key: 'xiongxian', name: '雄县', sub: {
+                    tabName: '村庄', selectItems: [
+                      { key: 'ljz', name: '李家寨' }
+                    ]
+                  }
+                }
+              ]
+            }
+          }
+        ]
+      }
     }
   ]
 }
-  
-]
+
+
 
 class ProductInfo extends React.Component {
   render() {
@@ -66,7 +90,7 @@ class ProductInfo extends React.Component {
           <div className={style.line}>
             <span className={style.pLabel}>配送</span>
             <span>至</span>
-            <div className={style.address}><AddressSelect citys={citys}/></div>
+            <div className={style.address}><AddressSelect addressTree={addressTree} /></div>
           </div>
           <div></div>
         </div>

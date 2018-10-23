a57029b516d03fb98254e40c7dd00b8bf8767d8c
feng_home@126.com <feng_home@126.com>
2018-10-24 07:51:42 +0800

    allLook

--- src/components/AllLook/AllLook.js
+++ src/components/AllLook/AllLook.js
@@ -20,16 +20,64 @@ class AllLook extends React.Component {
             <Row gutter={16}>
               <Col span={6}>
                 <div className={style.product}>
-                  <img className={style.cover} src="p1.jpg"/>
+                  <img className={style.cover} src="pp1.png" />
                   <div className={style.tag}>
                     <Tag text="今日特价" bColor="#e36844" />
                   </div>
                   <a>平板喷水拖把</a>
                   <div className={style.priceInf}>
-                    <span className={style.pri}></span>
-                    <span className={style.oPri}></span>
+                    <span className={style.pri}>¥88</span>
+                    <span className={style.oPri}>¥188</span>
                     <span className={style.icon}>
-
+                      <Icon type="shopping-cart" theme="outlined" />
+                    </span>
+                  </div>
+                </div>
+              </Col>
+              <Col span={6}v>
+                <div className={style.product}>
+                  <img className={style.cover} src="pp2.png" />
+                  <div className={style.tag}>
+                    <Tag text="今日特价" bColor="#e36844" />
+                  </div>
+                  <a>平板喷水拖把</a>
+                  <div className={style.priceInf}>
+                    <span className={style.pri}>¥88</span>
+                    <span className={style.oPri}>¥188</span>
+                    <span className={style.icon}>
+                      <Icon type="shopping-cart" theme="outlined" />
+                    </span>
+                  </div>
+                </div>
+              </Col>
+              <Col span={6}>
+                <div className={style.product}>
+                  <img className={style.cover} src="pp3.png" />
+                  <div className={style.tag}>
+                    <Tag text="今日特价" bColor="#e36844" />
+                  </div>
+                  <a>平板喷水拖把</a>
+                  <div className={style.priceInf}>
+                    <span className={style.pri}>¥88</span>
+                    <span className={style.oPri}>¥188</span>
+                    <span className={style.icon}>
+                      <Icon type="shopping-cart" theme="outlined" />
+                    </span>
+                  </div>
+                </div>
+              </Col>
+              <Col span={6}>
+                <div className={style.product}>
+                  <img className={style.cover} src="pp4.png" />
+                  <div className={style.tag}>
+                    <Tag text="今日特价" bColor="#e36844" />
+                  </div>
+                  <a>平板喷水拖把</a>
+                  <div className={style.priceInf}>
+                    <span className={style.pri}>¥88</span>
+                    <span className={style.oPri}>¥188</span>
+                    <span className={style.icon}>
+                      <Icon type="shopping-cart" theme="outlined" />
                     </span>
                   </div>
                 </div>
